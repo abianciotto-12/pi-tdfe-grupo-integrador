e4af665c31d015dd706530dddb05bd219983cca6
Carmen Suen Blanc <csuenblanc@Carmens-MacBook.local>
2026-06-06 18:49:53 -0300

    base de login y login.js

---
+++ js/login.js
@@ -0,0 +1,35 @@
+let formLogin = document.querySelector ('#formLogin')
+
+formLogin.addEventListener ('submit', function(e) {
+    e.preventDefault();
+
+    let inputEmail = document.querySelector('#inputEmail');
+    let inputPassword = document.querySelector('#inputPassword');
+
+    let errorEmail = document.querySelector('#errorEmail');
+    let errorPassword = document.querySelector('#errorPassword');
+
+    errorEmail.innerText = '';
+    errorPassword.innerText = '';
+
+    let hayErrores = false;
+
+    if (inputEmail.value === '') {
+        errorEmail.innerText = 'El email es obligatorio';
+        hayErrores = true;
+    }
+
+    if (inputPassword.value === '') {
+        errorPassword.innerText = 'La contraseña es obligatoria';
+        hayErrores = true;
+    } else if (inputPassword.value.length < 6) {
+        errorPassword.innerText = 'La contraseña debe tener al menos 6 caracteres';
+        hayErrores = true;
+    }
+
+    if (hayErrores === false){
+        localStorage.setItem('email', inputEmail.value);
+        window.location.href = './index.html';
+
+    }
+})
